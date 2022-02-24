8dce62990f6c7b82a3586f74cfd8ac2f2b776648
josh-leebrowne <josh.lv.lee@gmail.com>
2022-02-23 19:29:26 -0600

    app.js

---
+++ week1/Math/app.js
@@ -0,0 +1,8 @@
+const mathObj = require('./math')
+
+
+console.log(mathObj.sum(10,5))
+console.log(mathObj.sub(10,5))
+console.log(mathObj.mul(10,5))
+console.log(mathObj.div(10,5))
+
---
+++ week1/Math/math.js
@@ -0,0 +1,23 @@
+const sum = (a, b) => {
+    return a + b
+}
+
+const sub = (a, b) => {
+    return a - b
+}
+
+const mul = (a, b) => {
+    return a * b
+}
+
+const div = (a, b) => {
+    return a / b
+}
+
+module.exports = {
+    sum: sum,
+    sub: sub,
+    mul: mul,
+    div: div
+}
+
